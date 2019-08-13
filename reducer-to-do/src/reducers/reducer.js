12e71baf511865b06f764d3ac2934306f015b001
Reese Kunz <reesekunz21@gmail.com>
2019-08-13 12:29:57 -0700

    built out initial reducer

---
+++ reducer-to-do/src/reducers/reducer.js
@@ -0,0 +1,17 @@
+// need 2 things - initial state & reducer function
+
+export const initialState = {
+
+item: 'Learn about reducers',
+completed: false,
+id: 3892987589,
+
+}
+
+
+
+export const todoReducer = (state, action) => {
+    console.log(state, action);
+
+    default: return state;
+};
